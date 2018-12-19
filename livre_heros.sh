a829ecade7325f82539020546d0aea130d4fb32b
Cedro23 <cedric1lesueur@gmail.com>
2018-12-19 16:55:08 +0100

    script affichage de page

--- livre_heros.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-clear
-CUR_DIR=`dirname $0`
-
-if [ -z $1 ] || [ $1 -eq 1 ]
-then
-	cat $CUR_DIR/livre/page01.txt
-fi
---
+++ scripts/display_page.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+clear
+CUR_DIR=`dirname $0`
+
+cat $CUR_DIR/livre/page0$1.txt
