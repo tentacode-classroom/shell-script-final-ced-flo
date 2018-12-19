e39d844a05ab9be794e806863cdfbec998ff9073
Cedro23 <cedric1lesueur@gmail.com>
2018-12-19 17:15:57 +0100

    fini ?

--- scripts/display_page.sh
+++ scripts/display_page.sh
@@ -3,4 +3,25 @@
 clear
 CUR_DIR=`dirname $0`
 
+function NextPage()
+{
+	read -p "Votre choix : " CHOICE
+	$0 ${TAB_PAGE[$CHOICE-1]}
+}
+
 cat $CUR_DIR/../livre/page0$1.txt
+
+if [ $1 -eq 1 ]
+then
+	TAB_PAGE=(2 3 4)
+elif [ $1 -eq 3 ]
+then
+	TAB_PAGE=(5 6 4)
+elif [ $1 -eq 6 ]
+then
+	TAB_PAGE=(7 8 4)
+else
+	exit
+fi
+
+NextPage
---
+++ scripts/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+CUR_DIR=`dirname $0`
+
+./$CUR_DIR/display_page.sh 1
